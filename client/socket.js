2fae489331521f8fb9614810fcab140616ee9f24
Ynan8 <111168346+Ynan8@users.noreply.github.com>
2024-03-17 16:22:05 +0700

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-   return io("https://ezclass-webapp.vercel.app/", options)    ;
+   return io("http://localhost:8000", options)    ;
 }
