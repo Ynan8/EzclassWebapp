b5d01b047cad1a888a5456a99bba6e5ad9be1e86
Ynan8 <111168346+Ynan8@users.noreply.github.com>
2024-03-17 15:19:46 +0700

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -3,9 +3,9 @@ import { io } from 'socket.io-client';
 export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        'reconnectionAttempts':
-        'timeout': 10000,
-        'transports': ['websocket'],
+        'reconnectionAttempt': 'infinity',
+        timeout: 10000,
+        transports: ['websocket'],
     };
    return io(process.env.NEXT_PUBLIC_API, options)    ;
 }
