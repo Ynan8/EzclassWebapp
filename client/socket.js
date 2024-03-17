254b777b0a193f3cbb6be700d99ed75a25e7cc15
Ynan8 <111168346+Ynan8@users.noreply.github.com>
2024-03-17 15:16:22 +0700

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         'timeout': 10000,
         'transports': ['websocket'],
     };
-    return io("http://localhost:8000", options);
+   return io(process.env.NEXT_PUBLIC_API, options)    ;
 }
