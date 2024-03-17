25924db25404a6abc9498106668904150b9bf11c
Ynan8 <111168346+Ynan8@users.noreply.github.com>
2024-03-17 15:11:10 +0700

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -3,9 +3,9 @@ import { io } from 'socket.io-client';
 export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        'reconnectionAttempt': 'infinity',
-        timeout: 10000,
-        transports: ['websocket'],
+        'reconnectionAttempts':
+        'timeout': 10000,
+        'transports': ['websocket'],
     };
-    return io( "http://localhost:8000", options)
+    return io("http://localhost:8000", options);
 }
